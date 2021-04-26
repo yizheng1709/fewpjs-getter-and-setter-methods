727880ee129601bfca940d8235485bfae8fbcac3
Eva <yi.zheng1709@gmail.com>
2021-04-26 12:32:48 -0400

    commit tree d7fa2427d804d95fbeb6b4c7c3b8ef9b6084a066

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+
+    // get radius() {
+    //     return this.radius
+    // }
+
+    // set radius(r) {
+    //     this.radius = r
+    // }
+
+    get diameter() {
+        return 2 * this.radius 
+    }
+
+    set diameter(diam) {
+        this.radius = diam/2
+    }
+
+    get circumference() {
+        return Math.PI * this.diameter
+    }
+
+    set circumference(num) {
+        this.radius = num/(Math.PI * 2)
+    }
+
+    get area() {
+        return Math.PI * this.radius**2
+    }
+}
